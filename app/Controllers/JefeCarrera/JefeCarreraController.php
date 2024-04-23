4359625ff5063bcbc171e97a2e57c8a15367e7e3
edgardegantea <edgar.degante.a@gmail.com>
2024-04-23 12:07:20 -0600

    Inicio de proyecto sicac

---
+++ app/Controllers/JefeCarrera/JefeCarreraController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers\JefeCarrera;
+
+use App\Controllers\BaseController;
+
+class JefeCarreraController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
